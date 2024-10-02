ef4380548d8b72b63ef0196f60eccb173e764de9
sophieouioui <sofapiano20@gmail.com>
2024-10-02 21:13:15 +0300

    divided draw_line function into draw_line_c and draw_line, added y=x/3 graph

--- lab.py
+++ lab.py
@@ -1,7 +1,11 @@
 import time
 
-def draw_line(offset=0, color=222):
+def draw_line_с(offset=0, color=222):
     print(f'{"   " * offset}\x1b[48;5;{color}m{"   "}\x1b[0m{"   " * (4-offset*2)}\x1b[48;5;{color}m{"   "}\x1b[0m{"   " * offset}' * 2)
+
+def draw_line(offset=0, color=222, length=1):
+    line = '   ' * length
+    print(f'{"   " * offset}\x1b[48;5;{color}m{line}\x1b[0m')
 
 def circles():
     height = 6
@@ -9,14 +13,15 @@ def circles():
     step = 1
     color = 255
     for line in range(height-1):
-        draw_line(offset, color)
+        draw_line_с(offset, color)
         if line == center:
-            draw_line(offset, color)
+            draw_line_с(offset, color)
             offset += step
         elif line < center:
             offset -= step
         else:
             offset += step
+
 
 #страна
 red = f"\x1b[48;5;{1}m{' ' * 3}\x1b[0m"
@@ -30,3 +35,7 @@ print(red * 5)
 #узор
 if __name__ == '__main__':
     circles()
+
+print('если принять 6 клеточек (1 клеточка = 3 пробела в ширину) за единицу:')
+for i in range(9):
+    draw_line(9*3-i*3, 1, 3)
